cbcae094b4d241d134c5379e6f78755cbe0458fb
Tim Truong <95136892+teateatime@users.noreply.github.com>
2022-06-15 22:30:33 -0400

    Updated Stuff

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ function check() {
         // or not. Because it crashes if i enter a large difference greater than 600 but
         // that could be my laptop, but to make sure it definitely works on all devices i set it to this,
     } else if (((max_r_val - min_r_val) > 600) || ((max_c_val - min_c_val) > 600)) {
-        div.innerHTML = "Please enter a range where the difference between the max and min is <= 800";
+        div.innerHTML = "Please enter a range where the difference between the max and min is <= 600";
         output.style.display = "none";
         scroll.style.display = "none";
         div.style.display = "block";
